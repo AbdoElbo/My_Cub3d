f4dc18c9aa7b832c02cf677a2283be02848c5daa
Luca Pieck <lpieck@f1r6s19.codam.nl>
2026-07-08 17:40:58 +0200

    betere minimap, verschillende textures op walls2

--- Objects/Enemy/enemy.d
+++
@@ -1,9 +0,0 @@
-Objects/Enemy/enemy.o: Enemy/enemy.c Enemy/enemy.h \
-  Enemy/../Libraries/Mlx42/include/MLX42/MLX42.h Enemy/../Cubed.h \
-  Enemy/../Libraries/Libft/libft.h Enemy/../Libraries/Macros.h \
-  Enemy/../Parsing/Parsing.h Enemy/../Parsing/../Cubed.h \
-  Enemy/../Map_validity/Validity.h Enemy/../Map_validity/../Cubed.h \
-  Enemy/../Plane/Plane.h Enemy/../Plane/../Cubed.h \
-  Enemy/../Game_Dev/Game_Dev.h Enemy/../Game_Dev/../Cubed.h \
-  Enemy/../Rendering/Rendering.h Enemy/../Rendering/../Cubed.h \
-  Enemy/../Enemy/enemy.h
--- Objects/Main.d
+++
@@ -1,7 +0,0 @@
-Objects/Main.o: Main.c Cubed.h Libraries/Libft/libft.h \
-  Libraries/Mlx42/include/MLX42/MLX42.h Libraries/Macros.h \
-  Parsing/Parsing.h Parsing/../Cubed.h Map_validity/Validity.h \
-  Map_validity/../Cubed.h Plane/Plane.h Plane/../Cubed.h \
-  Game_Dev/Game_Dev.h Game_Dev/../Cubed.h Rendering/Rendering.h \
-  Rendering/../Cubed.h Enemy/enemy.h \
-  Enemy/../Libraries/Mlx42/include/MLX42/MLX42.h
